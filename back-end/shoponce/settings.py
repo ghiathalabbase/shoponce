9c73b6d25687857d82d0c81cddd929703af35877
Othman0963 <hamodyothman05@gmail.com>
2023-01-14 23:17:24 +0300

    Creating Data Base

--- back-end/shoponce/settings.py
+++ back-end/shoponce/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'DataBase.apps.DatabaseConfig'
 ]
 
 MIDDLEWARE = [
@@ -116,6 +118,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+SHOP_MEDIA_URL = 'shop_imgs'
+PRODUCT_MEDIA_URL = 'product_imgs'
+USER_MEDIA_URL = 'user_imgs'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
