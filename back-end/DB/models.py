bc692b12418d7fa5cdbbaf31229b8a93c2dfdc81
Othman0963 <hamodyothman05@gmail.com>
2023-01-15 16:14:15 +0300

    edits

--- back-end/DB/models.py
+++ back-end/DB/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import MinValueValidator, MaxValueValidator
+
 STORES_IMAGES_DIR = 'stores images'
 PRODUCTS_IMAGES_DIR = 'products images'
 # Create your models here.
@@ -16,39 +18,64 @@ class User(AbstractUser):
   # REQUIRED_FIELDS=['username']
 class Country(models.Model):
   name = models.CharField(max_length=50)
+
+  def __str__(self):
+      return self.name
+
 class City(models.Model):
   name = models.CharField(max_length=50)
   country = models.ForeignKey(Country, on_delete=models.CASCADE)
+
+  def __str__(self):
+      return self.name
 
 class Categorey(models.Model):
   name = models.CharField(max_length=20)
   stores = models.ManyToManyField('Store', through='CategoriesStores')
 
+  def __str__(self):
+      return self.name
+
 class FavouriteCategories(models.Model):
   categorey = models.ForeignKey(Categorey, on_delete=models.CASCADE)
   user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+  def __str__(self):
+      return self.user.username + " | " + self.categorey.name
 
 class Tag(models.Model):
   name = models.CharField(max_length=30)
   categorey = models.ForeignKey(Categorey, on_delete=models.SET_NULL, null =True)
   stores = models.ManyToManyField('Store', through='TagsStores')
 
+  def __str__(self):
+      return self.name
+
 class FavouriteTags(models.Model):
   tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
   user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+  def __str__(self):
+      return self.user.username + " | " + self.tag.name
     
 class Store(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
-    # rate = models.FloatField(min=0, max=5, default=0)
+    rate = models.FloatField(validators=[MinValueValidator(0.0), MaxValueValidator(5.0)], default=0)
     description = models.TextField(null=True, blank=True)
     # main_image = models.ImageField(upload_to='stores images', null=True, blank=True)
     logo = models.ImageField(upload_to=STORES_IMAGES_DIR, null=True, blank=True)
     categories = models.ManyToManyField(Categorey, through='CategoriesStores')
 
+    def __str__(self):
+      return self.name
+
 class CategoriesStores(models.Model):
   categorey = models.ForeignKey(Categorey, on_delete=models.CASCADE)
   store = models.ForeignKey(Store, on_delete=models.CASCADE)
+
+  def __str__(self):
+      return self.store.name + " | " + self.categorey.name
 
 class TagsStores(models.Model):
   tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
@@ -63,26 +90,43 @@ class Branch(models.Model):
   city = models.ForeignKey(City, on_delete=models.SET_NULL, null=True)
   is_centeral = models.BooleanField(default=False)
 
+  def __str__(self):
+      return self.store.name + " | " + self.city.name
+
 class BranchImages(models.Model):
   picture = models.ImageField(upload_to=STORES_IMAGES_DIR, null=True,blank=True)
   branch = models.ForeignKey(Branch, on_delete=models.CASCADE)
 
 class Product(models.Model):
   name = models.CharField(max_length=80)
-  rate = models.FloatField()
+  rate = models.FloatField(validators=[MinValueValidator(0.0), MaxValueValidator(5.0)], default=0)
   branch = models.ForeignKey(Branch, on_delete=models.CASCADE)
-  # price = models.IntegerField(min=0)
+  price = models.IntegerField(validators=[MinValueValidator(0.0)])
   main_image = models.ImageField(upload_to=PRODUCTS_IMAGES_DIR, null=True, blank=True)
   description = models.TextField(null=True)
   category = models.ForeignKey(Categorey, on_delete=models.SET_NULL, null=True)
   tag = models.ForeignKey(Tag, on_delete=models.SET_NULL, null=True)
 
+  def __str__(self):
+      return self.name
+
+# Need Discuss
+class Discount(models.Model):
+    value = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(100)])
+    duration = models.FloatField(null=True)
+    # created_at = models.DateTimeField(auto_now_add=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+    def __str__(self):
+      return self.product.name + " | " + str(self.value)
+
 class ProductImages(models.Model):
   picture = models.ImageField(upload_to=PRODUCTS_IMAGES_DIR, null=True, blank=True)
   product = models.ForeignKey(Product, on_delete=models.CASCADE)
 
-
-
 class FavouriteProducts(models.Model):
   user = models.ForeignKey(User, on_delete=models.CASCADE)
-  produt = models.ForeignKey(Product,on_delete=models.CASCADE)
+  product = models.ForeignKey(Product,on_delete=models.CASCADE)
+
+  def __str__(self):
+      return self.user.username + " | " + self.product.name
